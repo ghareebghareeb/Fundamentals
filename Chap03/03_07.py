a95e84285b0a3a2e5e89958e750abaf99bcd28ab
Ghareeb Elshaikh <34264398+ghareebghareeb@users.noreply.github.com>
2022-09-05 15:31:45 +0200

    Adding the stage2 of files

---
+++ Chap03/03_07.py
@@ -0,0 +1,14 @@
+print("Challenge 1:")
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message) # show the message on the screen
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Challenge complete!")
---
+++ Chap03/03_08_solution
@@ -0,0 +1,4 @@
+Challenge 1:
+This is going to be tricky ;)
+2**3 = 8
+Challenge complete!
